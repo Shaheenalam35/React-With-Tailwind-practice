ceabf808f43ccaef3352e6e1b9f336c70c901d1d
Md Shaheen Alam <shaheenjoy641@gmail.com>
2022-03-29 22:28:42 +0200

    last update cart

--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -1,11 +1,44 @@
 import React from "react";
+
 import PricingOption from "../PricingOption/PricingOption";
 
 const Pricing = () => {
   const pricingOptions = [
-    { id: 1, name: "Free", price: 0 },
-    { id: 2, name: "Regular", price: 9.99 },
-    { id: 3, name: "Premium", price: 19.99 },
+    {
+      id: 1,
+      name: "Free",
+      price: 0,
+      benefits: [
+        "Life time free",
+        "unlimited deals",
+        "localised deals",
+        "fantastic offer",
+        "crazy deals",
+      ],
+    },
+    {
+      id: 2,
+      name: "Regular",
+      price: 9.99,
+      benefits: [
+        "regular useable",
+        "unlimited deals",
+        "fantastic offer",
+        "crazy deals",
+      ],
+    },
+    {
+      id: 3,
+      name: "Premium",
+      price: 19.99,
+      benefits: [
+        "Life time free",
+        "unlimited offer",
+        "localised deals",
+        "fantastic deals",
+        "exciting deals",
+      ],
+    },
   ];
   return (
     <div className="bg-indigo-300 p-4 mt-8">
@@ -21,6 +54,9 @@ const Pricing = () => {
         {pricingOptions.map((option) => (
           <PricingOption key={option.id} option={option}></PricingOption>
         ))}
+      </div>
+      <div>
+        <h3 className="text-xl text-left">Benefits</h3>
       </div>
     </div>
   );
--- src/components/PricingOption/PricingOption.js
+++ src/components/PricingOption/PricingOption.js
@@ -1,7 +1,9 @@
+import { ArrowRightIcon } from "@heroicons/react/solid";
 import React from "react";
+import Benefits from "../Benefit/Benefits";
 
 const PricingOption = (props) => {
-  const { name, price } = props.option;
+  const { name, price, benefits } = props.option;
   return (
     <div className="bg-white p-4 rounded">
       <h3 className="bg-indigo-200 rounded text-xl font-bold p-2">{name}</h3>
@@ -9,6 +11,14 @@ const PricingOption = (props) => {
         <span className="text-5xl font-bold">{price}</span>
         <span className="text-xl font-bold text-gray-500">/mo</span>
       </p>
+      <div>
+        {benefits.map((benefit) => (
+          <Benefits benefit={benefit}></Benefits>
+        ))}
+      </div>
+      <button className="bg-green-500 justify-center flex w-full py-2 font-bold rounded mt-6 text-white hover:text-green-700 ">
+        Buy Now<ArrowRightIcon className="w-5 h-5 ml-2 "></ArrowRightIcon>
+      </button>
     </div>
   );
 };
